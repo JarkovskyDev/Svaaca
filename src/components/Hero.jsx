02c6eedd2ea59b57acf0b379c87851eea49d4b64
Dušan Jarkovský <jarkovsky.dev@gmail.com>
2023-12-10 23:11:01 +0100

    Web vol 2 complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import BGIMG from "../assets/Hero_bg_img.webp"
-import { IoIosArrowDown } from "react-icons/io";
+import image_bg from "../assets/Hero_bg_img.webp"
+import { IoIosArrowDropdown } from "react-icons/io";
 import { Link } from 'react-scroll';
 
 
 const Hero = () => {
   return (
-    <div className='w-full min-h-screen flex items-center justify-center bg-cover bg-center' style={{ backgroundImage: `url(${BGIMG})` }}>
+    <div className='w-full min-h-screen flex items-center justify-center bg-cover bg-center' style={{ backgroundImage: `url(${image_bg})` }}>
       <Link to='about' smooth={true} duration={500}>
-        <IoIosArrowDown size={75} color='white' className='cursor-pointer absolute bottom-5 hover:border-solid border-2 rounded-full hover:scale-110 duration-500'/>
+        <IoIosArrowDropdown size={75} color='white' className='cursor-pointer absolute bottom-5 hover:scale-110 duration-500'/>
       </Link>
     </div>
   )
